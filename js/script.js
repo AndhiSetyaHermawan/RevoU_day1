b6be975e0dbbde497571016d87590fc667412a8f
Andhi Setya Hermawan <hermawan.andhisetya@gmail.com>
2023-06-18 12:23:48 +0700

    Update Lagi

--- js/script.js
+++ js/script.js
@@ -1,28 +1,42 @@
 document.getElementById("temperatureForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Menghentikan pengiriman formulir
-  
-    var temperature = parseFloat(document.getElementById("temperature").value);
-    var conversionType = document.getElementById("conversionType").value;
-    var resultElement = document.getElementById("result");
-    var result;
-  
-    if (isNaN(temperature)) {
-      result = "Masukkan suhu yang valid!";
-    } else {
-      if (conversionType === "celcius") {
-        result = "Hasil konversi: " + convertToFahrenheit(temperature) + " Fahrenheit";
-      } else if (conversionType === "fahrenheit") {
-        result = "Hasil konversi: " + convertToCelcius(temperature) + " Celcius";
-      }
-    }
-  
-    resultElement.innerHTML = result;
-  });
-  
-  function convertToFahrenheit(celcius) {
-    return (celcius * 9/5) + 32;
+  event.preventDefault(); // Menghentikan pengiriman formulir
+
+  var temperature = parseFloat(document.getElementById("temperature").value);
+  var resultElement = document.getElementById("result");
+  var result;
+
+  if (isNaN(temperature)) {
+    result = "Masukkan suhu yang valid!";
+  } else {
+    result = "Hasil konversi: " + convertToFahrenheit(temperature) + " Fahrenheit";
   }
-  
-  function convertToCelcius(fahrenheit) {
-    return (fahrenheit - 32) * 5/9;
-  }  
+
+  resultElement.innerHTML = result;
+});
+
+function convertToFahrenheit(celcius) {
+  return (celcius * 9/5) + 32;
+}
+
+function resetForm() {
+  document.getElementById("temperatureForm").reset();
+  document.getElementById("result").innerHTML = "";
+}
+
+function reverseConversion() {
+  var temperature = parseFloat(document.getElementById("temperature").value);
+  var resultElement = document.getElementById("result");
+  var result;
+
+  if (isNaN(temperature)) {
+    result = "Masukkan suhu yang valid!";
+  } else {
+    result = "Hasil konversi: " + convertToCelcius(temperature) + " Celcius";
+  }
+
+  resultElement.innerHTML = result;
+}
+
+function convertToCelcius(fahrenheit) {
+  return (fahrenheit - 32) * 5/9;
+}
