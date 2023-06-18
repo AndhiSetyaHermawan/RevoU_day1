06cdd0ce5031a3535f934c3cfc70841dc4da5729
Andhi Setya Hermawan <hermawan.andhisetya@gmail.com>
2023-06-18 13:19:15 +0700

    Update versi 2

--- js/script.js
+++ js/script.js
@@ -8,14 +8,14 @@ document.getElementById("temperatureForm").addEventListener("submit", function(e
   if (isNaN(temperature)) {
     result = "Masukkan suhu yang valid!";
   } else {
-    result = "Hasil konversi: " + convertToFahrenheit(temperature).toFixed(2) + " Fahrenheit";
+    result = "Hasil konversi: " + convertToFahrenheit(temperature) + " Fahrenheit";
   }
 
   resultElement.innerHTML = result;
 });
 
-function convertToFahrenheit(celsius) {
-  return (celsius * 9/5) + 32;
+function convertToFahrenheit(celcius) {
+  return (celcius * 9/5) + 32;
 }
 
 function resetForm() {
@@ -31,12 +31,12 @@ function reverseConversion() {
   if (isNaN(temperature)) {
     result = "Masukkan suhu yang valid!";
   } else {
-    result = "Hasil konversi: " + convertToCelsius(temperature).toFixed(2) + " Celcius";
+    result = "Hasil konversi: " + convertToCelcius(temperature) + " Celcius";
   }
 
   resultElement.innerHTML = result;
 }
 
-function convertToCelsius(fahrenheit) {
+function convertToCelcius(fahrenheit) {
   return (fahrenheit - 32) * 5/9;
 }
